914caee679cd2688c42d20f9a54647ed62a94920
Emanuel <maicol28@gmail.com>
2026-05-31 15:14:00 -0400

    fix: modificacion del entrypoint para no tener problemas con los permisos.

--- docker/php-apache/entrypoint.sh
+++ docker/php-apache/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Configurando permisos..."
 
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache
 
 echo "Iniciando Apache..."
 
