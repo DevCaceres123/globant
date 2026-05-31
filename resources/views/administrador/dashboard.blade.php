aedebacbba07b75657f9942af8c3dac1e8f59f59
Emanuel <maicol28@gmail.com>
2026-05-30 20:26:19 -0400

    feat: se realizo la configuracion inicial, para adminlte.

---
+++ resources/views/administrador/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('adminlte::page')
+
+@section('title', 'Panel de control')
+
+<!-- @section('css')
+<link rel="stylesheet" href="/css/custom.css">
+@stop -->
+
+@section('content_header')
+<h1 class="text-center">PANEL DE ADMINISTRACION GLOBANT SRL</h1>
+@stop
+
+@section('content')
+<div class="small-box bg-info">
+    <div class="inner">
+        <h3>150</h3>
+        <p>Usuarios</p>
+    </div>
+    <div class="icon">
+        <i class="fas fa-users"></i>
+    </div>
+</div>
+@stop
+
+<!-- @section('js')
+<script>
+    console.log('Hola AdminLTE');
+</script>
+@stop -->
